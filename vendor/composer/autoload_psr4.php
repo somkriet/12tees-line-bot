5d08be05d146c4e036ca7460647138e98b59c899
jay <somkriet5769@gmail.com>
2020-02-21 16:58:40 +0700

    update composer

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Cowsayphp\\' => array($vendorDir . '/alrik11es/cowsayphp/src'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit443561a412b7c1b3399899586a4c9643
 {
+    public static $prefixLengthsPsr4 = array (
+        'C' => 
+        array (
+            'Cowsayphp\\' => 10,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Cowsayphp\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/alrik11es/cowsayphp/src',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit443561a412b7c1b3399899586a4c9643::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit443561a412b7c1b3399899586a4c9643::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
