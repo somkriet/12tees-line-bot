70049ec196cc5c8bae273e2af90cfd95b5ad8836
jay <somkriet5769@gmail.com>
2020-02-21 14:05:49 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -57,6 +57,7 @@ if (sizeof($request_array['events']) > 0 ) {
                                 // echo "$i x $multi = ".$i*$multi. "<br>"; 
 
                                 $respMessage = 'สูตรคูณแม่' . $event['message']['text'] . ' $i x $multi = '.$i*$multi '<br>';
+                                }
 
                                 $data = [
                                      'replyToken' => $reply_token,
