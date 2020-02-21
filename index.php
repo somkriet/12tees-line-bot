c1d7ff5247419570fc260908f53f0af308758191
jay <somkriet5769@gmail.com>
2020-02-21 17:02:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,9 @@
 
 require('../vendor/autoload.php');
 
+$app->get('/cowsay', function() use($app) {
+  $app['monolog']->addDebug('cowsay');
+  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+});
 
 ?>
